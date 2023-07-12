a728e2682c13d0cad453d0508631df13b3c5766a
Daniela Rattin <daniela@ecosis.com.br>
2023-07-12 15:53:27 -0300

    Cria botão detalhes do filme salvo

---
+++ src/pages/Favorito/index.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import "./favorito.css";
+import { Link } from 'react-router-dom';
+
+
+function Favorito() {
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@primeflix");
+        setFilmes(JSON.parse(minhaLista) || [])
+    }, [])
+
+    return(
+        <div className='meus-filmes'>
+
+            <h1>Meus filmes favoridos</h1>         
+                <ul>
+                    {filmes.map((item) => {
+                        return(
+                            <li key={item.id} >
+                                <span>{item.title}</span>
+                                <div>
+                                    <Link to={`/filme/${item.id}`} >Ver detalhes</Link>
+                                    <button>Excluir da Lista</button>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+
+        </div>
+    )
+}
+export default Favorito;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Header from './components/Header';
 import Erro from './pages/Erro';
+import Favorito from './pages/Favorito';
+
 
 function RoutesApp() {
     return(
@@ -13,6 +15,7 @@ function RoutesApp() {
         <Routes>
             <Route path='/' element={ <Home/> } >Home</Route>
             <Route path='/filme/:id' element={ <Filme/> } >Filme</Route>
+            <Route path='/favorito' element={ <Favorito/> } >Favorito</Route>
             
             <Route path='*' element={ <Erro/>} ></Route>
         </Routes>
