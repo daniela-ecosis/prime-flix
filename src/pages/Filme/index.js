8a77f8a14d3df443170cb74e94a3bc1c9ba34d6b
Daniela Rattin <daniela@ecosis.com.br>
2023-07-12 15:12:14 -0300

    Cria botão do trailer e salva filmes na lista

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,93 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import api from '../../services/api'
+import './filme-info.css'
+
+
 function Filme() {
+
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "fd0be2e2611fbac51cb4e5a706c1aa36",
+                    language: "pt-br",
+                }
+            })
+            .then((response)=> {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=> {
+                console.log("FILME NÃO ENCONTRADO");
+                navigate("/", { replace: true });
+                return;
+            })
+        }
+    
+        loadFilme();
+
+        return() => {
+            console.log("COMPONENTE DESMONTADO")
+        }
+    }, [navigate,id])
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+        let filmesSalvos = JSON.parse(minhaLista) || []; 
+        const hasFilmes = filmesSalvos.some( (filmesSalvos) => filmesSalvos.id === filme.id )
+
+        if(hasFilmes) {
+            alert("esse filme já está  na lista");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        alert("Filme salvo com sucesso")
+
+    }
+
+    if(loading)
     return(
-        <h1>Bem vindo a página detalhes do Filme</h1>
-    );
+        <div className="filme-info">
+            <h1>Carregando detalhes...</h1>
+        </div>
+    )
+
+    return (
+        <div className="filme-info" >
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} ></img>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10 </strong>
+
+            <div className="area-buttons">
+                <button onClick={salvarFilme} >Salvar</button>
+                <button>
+                    <a href={`https://youtube.com/results?search_query=${filme.title}`} target="blank" rel="noreferrer">
+                        Trailer
+                    </a>
+
+                </button>
+
+            </div>
+
+        </div>
+
+    )
+
 }
+
+
 
 export default Filme;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 function Home() {
 
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=> {
 
@@ -15,16 +18,39 @@ function Home() {
                     page: 1,
                 }
             })
-            console.log(response);
+         // console.log(response.data.results.slice(0,10));
+          setFilmes(response.data.results.slice(0,10));
+          setLoading(false);
         }
 
         loadFilmes();
 
     }, [])
 
+if(loading) {
     return(
-        <div>
-            <h1>Bem vindo a página Home</h1>
+        <div className="loading">
+         <h2>Carregando filmes...</h2>   
+        </div>
+
+    )
+}
+
+
+    return(
+        <div className="container">
+            <div className="lista-filmes">
+            {filmes.map((filme) => {
+                return (
+                <article key={filme.id} >
+                    <strong>{filme.title}</strong><br/>
+                    <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} ></img>
+                   
+                    <Link to={`/filme/${filme.id}`} >Acessar</Link>
+                </article>
+                ) 
+            })}
+            </div>
         </div>
     )
 }
